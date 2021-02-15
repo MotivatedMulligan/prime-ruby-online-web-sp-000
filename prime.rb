1d35d0c44e458f823b94aa347ccb80287c45dbb3
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-15 20:31:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
 # Add  code here!
-def prime?(array)
-(2..(array/2)).none? {|n| return false if array <= 1 || array % n == 0}
+#def prime?(array)
+#(2..(array/2)).none? {|n| return false if array <= 1 || array % n == 0}
+#end
 
+n = 2
+while n < array
+   return false if array <= 1 || array % n == 0
+  n += 1
 end
-
-#n = 2
-#while n < array
-#   return false if array <= 1 || array % n == 0
-#  n += 1
-#end
 #return true
-#end
+end
