e58e82b71131594e74ecef56160b4f84a8cddbe0
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-15 19:51:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(array)
 #array.each{|n|aseq == 2 * (n / 2)}
-(2..(num - 1)).each do |n|
-   return false if num % n == 0
+(2..(array - 1)).each do |n|
+   return false if array % n == 0
  end
  true
 end
