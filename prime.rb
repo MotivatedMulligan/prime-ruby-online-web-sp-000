7d6a02ace7e6e15dd3811cd140d3d9a0c24913a4
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-15 20:22:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,13 @@
 # Add  code here!
 def prime?(array)
-((2..(array - 1)).each {|n| return false if array % n == 0}).negative?
+#(2..(array - 1)).each {|n| return false if array % n == 0}
+#true
+#end
+
+n = 2
+while n < num
+  return false if num % n == 0
+  n += 1
+end
 true
 end
